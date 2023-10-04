9f6ffcb6c2dfc3d0c35acce961e2f6e044159540
Artem Demidenko <ar.demidenko@gmail.com>
2023-10-04 22:13:35 +0300

    dev version incomplete

---
+++ run_qodana.sh
@@ -0,0 +1,1 @@
+docker run --rm -it -v ./:/data/project/ -e QODANA_TOKEN="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmdhbml6YXRpb24iOiIzWDRSdyIsInByb2plY3QiOiJ6WmIyaiIsInRva2VuIjoiM0syeVkifQ.rHuCL2y1ymHToPqFSG5hJO0hpD-w6g7lI9HDRPiOdKs" -p 8080:8080  jetbrains/qodana-python-community:2023.2 --show-report
