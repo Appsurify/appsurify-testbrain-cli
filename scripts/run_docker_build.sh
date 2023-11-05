49f536d931214e38ead9406d97a0cc488f1613be
whenessel <whenessel@gmail.com>
2023-11-05 16:46:53 +0300

    Update Dockerfile

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -4,3 +4,5 @@ docker build -f compose/Dockerfile -t appsurifyinc/appsurify-testbrain-cli:dev .
 
 docker buildx build --platform linux/arm64 -f compose/Dockerfile -t appsurifyinc/appsurify-testbrain-cli:dev . --load
 docker buildx build --platform linux/amd64 -f compose/Dockerfile -t appsurifyinc/appsurify-testbrain-cli:dev . --load
+
+docker buildx build --platform linux/arm64,linux/amd64 -f compose/Dockerfile -t appsurifyinc/appsurify-testbrain-cli:dev . --load
