46f2bc03edde65d84e598025a2adeb5e63adf6b4
Artem Demidenko <ar.demidenko@appsurify.com>
2024-01-15 00:35:31 +0300

    Bump version: 2023.12.11 → 2024.1.6

--- src/testbrain/cli/__init__.py
+++ src/testbrain/cli/__init__.py
@@ -5,5 +5,5 @@ __path__ = extend_path(__path__, __name__)
 __title__ = "Appsurify Testbrain CLI"
 __name__ = "appsurify-testbrain-cli"
 __prog__ = "testbrain-cli"
-__version__ = "2023.12.11"
+__version__ = "2024.1.6"
 __build__ = "undefined"
