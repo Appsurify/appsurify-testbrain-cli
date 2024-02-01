c61e439f6968d19de0a5ee2e7e8fd4883530ad55
Artem Demidenko <ar.demidenko@appsurify.com>
2024-02-01 13:04:45 +0300

    CI: Add __testing__ flag
    Special variable for periodically re-run tests

--- push.sh
+++ push.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-testbrain repository git push --server https://demo.appsurify.com --token MTU6ZW9FZUxhcXpMZU9CdGZZVmZ4U3BFM3g5MmhVcDl5ZmQzampUWEM1SWRfNA --project 001TESTPROJECT --repo-name test --branch development --commit latest --number 5 -l DEBUG
-testbrain repository git push --server https://demo.appsurify.com --token MTU6ZW9FZUxhcXpMZU9CdGZZVmZ4U3BFM3g5MmhVcDl5ZmQzampUWEM1SWRfNA --project 001TESTPROJECT --branch development
-
-
 testbrain repository git push --server [server] --token [token] --project [project name] --branch [branch name]
